a3a3a7b426aa558bf5faec6ef242762ee0b16c4f
sodastereo <f_er_@hotmail.com>
2020-06-28 16:33:29 -0400

    updates

--- app.js
+++ app.js
@@ -11,24 +11,23 @@ function closeNav() {
   document.body.style.backgroundColor = 'white';
 }
 
-// images modal
-// Get the modal
+// // Modal images
+// // Get the modal
 var modal = document.getElementById('myModal');
+var i;
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
+var img = document.getElementsByClassName('myImg');
 var modalImg = document.getElementById('img01');
-var captionText = document.getElementById('caption');
-img.onclick = function () {
-  modal.style.display = 'block';
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-};
 
-// Get the <span> element that closes the modal
+for (i = 0; i < img.length; i++) {
+  img[i].onclick = function () {
+    modal.style.display = 'block';
+    modalImg.src = this.src;
+  };
+}
+
 var span = document.getElementsByClassName('close')[0];
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function () {
   modal.style.display = 'none';
 };
